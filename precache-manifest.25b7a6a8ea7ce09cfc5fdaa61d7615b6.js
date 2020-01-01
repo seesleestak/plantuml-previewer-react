75f84de400d3da624e2b37fa52d4e114d294c9af
Calvin Cieslak <calvin.cieslak@gmail.com>
2020-01-01 08:30:41 -0800

    Updates

---
+++ precache-manifest.25b7a6a8ea7ce09cfc5fdaa61d7615b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab665a29673c99b6ecc45811dfbb04c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2453e29a8eae7fc916fe",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "1c0c67f7debaac6b3759",
+    "url": "/static/css/main.9487ec70.chunk.css"
+  },
+  {
+    "revision": "2453e29a8eae7fc916fe",
+    "url": "/static/js/2.27f9f097.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.27f9f097.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c0c67f7debaac6b3759",
+    "url": "/static/js/main.c8e64d3e.chunk.js"
+  },
+  {
+    "revision": "97f7cc365d577071b774",
+    "url": "/static/js/runtime-main.92f5ac3f.js"
+  }
+]);
